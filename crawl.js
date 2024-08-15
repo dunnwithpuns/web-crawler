d6748c8b7908adb902200c690acbe4f83fe1c67a
dunnwithpuns <dunnwithpuns@gmail.com>
2024-08-15 12:09:22 -0500

    tried to use trim, but couldn't get it to work, ended just slicing the string for normalize

--- crawl.js
+++ crawl.js
@@ -1,5 +1,24 @@
 export { normalizeURL };
 
 function normalizeURL(URLstring) {
-	return URLstring
+
+	let URL = URLstring
+
+
+	if (URL.endsWith('/')) {
+		URL = URL.slice(0, -1)
+		console.log(URL)
+	}
+
+	if (URL.startsWith('https://')) {
+		URL = URL.slice(8)
+		console.log(URL)
+
+	} else if (URL.startsWith('http://')) {
+		URL = URL.slice(7)
+		console.log(URL)
+	}
+
+	return URL
 }
+
--- crawl.test.js
+++ crawl.test.js
@@ -2,8 +2,9 @@ import { normalizeURL } from './crawl.js'
 import { test, expect } from '@jest/globals'
 
 test('inputs 4 different paths, should equal same normalized path', () => {
-	const input = ''
-	const actual = normalizeURL(input)
-	const expected = ''
-	expect(actual).toEqual(expected);
+
+	const input = 'https://blog.boot.dev/path/'
+	const result = normalizeURL(input)
+	const expected = 'blog.boot.dev/path'
+	expect(result).toEqual(expected);
 })
