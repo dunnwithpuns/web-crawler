10d9b178744226bcafa131e8196d33a953014e85
dunnwithpuns <dunnwithpuns@gmail.com>
2024-08-19 12:08:54 -0500

    added main()

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import { argv, exit } from 'node:process';
+
+function main() {
+	if (argv.length < 3) {
+		console.log(`Please enter a base URL`);
+		process.exit = 1;
+	} else if (argv.length > 3) {
+		console.log(`Too many arguments`);
+		process.exit = 2;
+	} else {
+		console.log(`Started crawling at ${argv[2]}`);
+	}
+}
+
+main()
